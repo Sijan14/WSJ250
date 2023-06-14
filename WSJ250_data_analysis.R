37becc074744df8287771d74c680047ba2687dc6
Sijan14 <sisijan18@earlham.edu>
2023-06-14 15:15:32 -0400

    Added some more analysis and simplified some previous analysis for better results

--- WSJ250_data_analysis.R
+++ WSJ250_data_analysis.R
@@ -1,9 +1,12 @@
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("forcats")
+install.packages("data.table")
 library(tidyverse)
 library(ggplot2)
 library(forcats)
+library(stringr)
+library(data.table)
 
 setwd("/Users/sijanikbal/Documents/JUST Capital/Data Project 1")
 df <- read_csv("wsj_250_best-managed_companies.csv")
@@ -30,27 +33,6 @@ industry %>%
   ggplot(aes(industry_average_overall_score, sector)) +
   geom_point()
 
-# Is there an industry-wide trend for various ratings?
-# customer_satisfaction
-cus_sat <- df %>% 
-  group_by(sector) %>% 
-  summarise(customer_satisfaction_mean = mean(customer_satisfaction)) %>% 
-  arrange(desc(customer_satisfaction_mean))
-head(cus_sat, 5)
-
-cus_sat %>% 
-  ggplot(aes(fct_rev(fct_reorder(sector, customer_satisfaction_mean)), customer_satisfaction_mean)) +
-  geom_col() +
-  theme(axis.text.x = element_text(angle = 90))
-
-# employee_engagement_and_development
-
-# innovation
-
-# social_respnsibility
-
-# financial strength
-
 # What is the industry average and standard deviation for various rating-scales?
 industry_average <- df %>% 
   group_by(sector) %>% 
@@ -66,4 +48,92 @@ industry_average <- df %>%
             overall_score_mean = mean(overall_score),
             overall_score_sd = sd(overall_score))
 
-#djg adg
+# Is there an industry-wide trend for various ratings?
+# customer_satisfaction
+cus_sat <- df %>% 
+  group_by(sector) %>% 
+  summarise(customer_satisfaction_mean = mean(customer_satisfaction)) %>% 
+  arrange(desc(customer_satisfaction_mean))
+head(cus_sat, 5)
+
+cus_sat %>% 
+  mutate(sector = fct_rev(fct_reorder(sector, customer_satisfaction_mean))) %>% 
+  ggplot(aes(sector, customer_satisfaction_mean)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90))
+
+# employee_engagement_and_development
+industry_average %>% 
+  mutate(sector = reorder(sector, employee_engagement_and_development_average, decreasing = TRUE)) %>% 
+  ggplot(aes(sector, employee_engagement_and_development_average)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90))
+
+# innovation
+industry_average %>% 
+  mutate(sector = reorder(sector, innovation_mean, decreasing = TRUE)) %>% 
+  ggplot(aes(sector, innovation_mean)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90))
+
+# social_respnsibility
+industry_average %>% 
+  mutate(sector = reorder(sector, social_responsibility_mean, decreasing = TRUE)) %>% 
+  ggplot(aes(sector, social_responsibility_mean)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90))
+
+# financial strength
+industry_average %>% 
+  mutate(sector = reorder(sector, financial_strength_mean, decreasing = TRUE)) %>% 
+  ggplot(aes(sector, financial_strength_mean)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90))
+
+# Which companies had the highest growth or decline in ratings from previous year?
+# Highest growth
+growth <- df[df$change_from_2019 %like% "up", ] # 142 companies improved their ratings from last year
+
+growth$change_from_2019 <- 
+  str_replace_all(growth$change_from_2019, "up ", "") %>% 
+  as.numeric()
+
+growth <- growth %>% 
+  arrange(desc(change_from_2019))
+
+growth <- rename(growth, growth_from_2019 = change_from_2019)
+head(growth, 10)
+
+growth %>%
+  top_n(10, growth_from_2019) %>%
+  arrange(desc(growth_from_2019)) %>%
+  ggplot(aes(reorder(company, growth_from_2019, decreasing = TRUE), growth_from_2019)) +
+  geom_col() +
+  xlab("Company") +
+  ylab("Growth from 2019") +
+  ggtitle("Top 10 Companies by Growth from 2019") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Highest decline
+decline <- df[df$change_from_2019 %like% "down", ] # 104 companies had a decline in their overall rating
+
+decline$change_from_2019 <- 
+  str_replace_all(decline$change_from_2019, "down ", "") %>% 
+  as.numeric()
+
+decline <- decline %>% 
+  arrange(desc(change_from_2019))
+
+decline <- rename(decline, decline_from_2019 = change_from_2019)
+head(decline, 10)
+
+decline %>%
+  top_n(10, decline_from_2019) %>%
+  arrange(desc(decline_from_2019)) %>%
+  ggplot(aes(reorder(company, decline_from_2019, decreasing = TRUE), decline_from_2019)) +
+  geom_col() +
+  xlab("Company") +
+  ylab("Decline from 2019") +
+  ggtitle("Top 10 Companies by Decline from 2019") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
